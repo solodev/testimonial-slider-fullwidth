c51703ef3108e0de2fdff2b37dabab5091525f07
SeanDanielsDigital <34247055+SeanDanielsDigital@users.noreply.github.com>
2018-02-20 16:09:51 -0500

    add main files

---
+++ test-slider-fullwidth.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".testimonial-carousel").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+    prevArrow: $(".testimonial-carousel-controls .prev"),
+    nextArrow: $(".testimonial-carousel-controls .next")
+  });
+});
